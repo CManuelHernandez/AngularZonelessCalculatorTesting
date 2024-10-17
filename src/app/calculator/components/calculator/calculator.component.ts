5095de055c7e473cf07ff9eb18cefea363dd5d65
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-17 09:46:03 +0200

    Calculator Service

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,9 +1,12 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
+  inject,
   viewChildren,
 } from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
 
 @Component({
   selector: 'calculator',
@@ -21,7 +24,17 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   // `,
 })
 export class CalculatorComponent {
+  private calculatorService = inject(CalculatorService);
+
   public calculatorButtons = viewChildren(CalculatorButtonComponent);
+
+  // get resultText() {
+  //   return this.calculatorService.reslutText;
+  // }
+
+  public resultText = computed(() => this.calculatorService.reslutText());
+  public subResultText = computed(() => this.calculatorService.subResultText());
+  public lastOperator = computed(() => this.calculatorService.lastOperator());
 
   handleClick(key: string) {
     console.log({ key });
