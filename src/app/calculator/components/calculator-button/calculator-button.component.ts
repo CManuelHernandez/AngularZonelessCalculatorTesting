a25fb5c07cf7dfd5dc3791ace5218512e6f9e56f
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-17 08:34:08 +0200

    OutputEmitterRef y Signal ViewChild

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,8 +1,11 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   HostBinding,
   input,
+  output,
+  viewChild,
   ViewEncapsulation,
 } from '@angular/core';
 
@@ -18,6 +21,9 @@ import {
   },
 })
 export class CalculatorButtonComponent {
+  public onClick = output<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+
   public isCommand = input(false, {
     transform: (value: boolean | string) =>
       typeof value === 'string' ? value === '' : value,
@@ -34,5 +40,12 @@ export class CalculatorButtonComponent {
 
   @HostBinding('class.w-2/4') get commandStyle() {
     return this.isDoubleSize();
+  }
+
+  handleClick() {
+    if (!this.contentValue()?.nativeElement) return;
+
+    const value = this.contentValue()!.nativeElement.innerText;
+    this.onClick.emit(value.trim());
   }
 }
