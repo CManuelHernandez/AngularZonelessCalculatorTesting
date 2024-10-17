bb7fedbb5b2816f33ae7082d161a3c717a53a003
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-17 11:55:59 +0200

    Pruebas en servicios con señales

---
+++ src/app/calculator/services/calculator.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+import { CalculatorService } from './calculator.service';
+
+describe('CalculatorService', () => {
+  let service: CalculatorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CalculatorService);
+  });
+
+  it('shold be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('shold be create with default values', () => {
+    expect(service.resultText()).toBe('0');
+    expect(service.subResultText()).toBe('0');
+    expect(service.lastOperator()).toBe('+');
+  });
+});
